f789dc06e53f575af595266d8cf95905d785ae76
kumahacker <kumahavana@gmail.com>
2019-07-28 00:47:55 -0400

    update

--- scripts.js
+++ scripts.js
@@ -1,16 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.fixed-action-btn');
-  var instances = M.FloatingActionButton.init(elems, {
-    direction: 'top',
-    hoverEnabled: false,
-    toolbarEnabled: false
-  });
-});
-
-// Or with jQuery
-
 $(document).ready(function(){
-  $('.fixed-action-btn').floatingActionButton();
+  $('.fixed-action-btn').floatingActionButton({
+    direction: 'top',
+    hoverEnabled: false
+  });
 });
 
 function ucwords(str){
