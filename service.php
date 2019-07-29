5e1976efa834cb02474a4224774ab77afa0fb893
kumahacker <kumahavana@gmail.com>
2019-07-29 02:51:26 -0400

    update

--- service.php
+++ service.php
@@ -106,9 +106,7 @@ class ClimaService extends ApretasteService
             'data'      => $data,
             'fcast'     => $fCast,
             'provinces' => array_keys($province)
-        ], array_map(static function ($value) {
-            return __DIR__."/icons/$value.png";
-        }, explode(',', '01d,02d,03d,04d,09d,10d,11d,50d,01n,02n,03n,04n,09n,10n,11n,50n')));
+        ]);
     }
 
     /**
