808c65784ca41bca099bd2a54d4a46aa77be4672
kumahacker <kumahavana@gmail.com>
2020-01-19 17:38:56 -0500

    fix style

--- weatherCache.php
+++ weatherCache.php
@@ -16,6 +16,7 @@
  */
 use Cmfcmf\OpenWeatherMap;
 use Cmfcmf\OpenWeatherMap\AbstractCache;
+
 /**
  * Cache implementation.
  *
@@ -23,56 +24,56 @@ use Cmfcmf\OpenWeatherMap\AbstractCache;
  */
 class OWMCache extends AbstractCache
 {
-    protected $tmp;
-    public function __construct()
-    {
-        $this->tmp = sys_get_temp_dir();
-    }
-    private function urlToPath($url)
-    {
-        $dir = $this->tmp . DIRECTORY_SEPARATOR . "OpenWeatherMapPHPAPI";
-        if (!is_dir($dir)) {
-            mkdir($dir);
-        }
-        $path = $dir . DIRECTORY_SEPARATOR . md5($url);
-        return $path;
-    }
-    /**
-     * @inheritdoc
-     */
-    public function isCached($url)
-    {
-        $path = $this->urlToPath($url);
-        if (!file_exists($path) || filectime($path) + $this->seconds < time()) {
-            //echo "Weather data is NOT cached!\n";
-            return false;
-        }
-        //echo "Weather data is cached!\n";
-        return true;
-    }
-    /**
-     * @inheritdoc
-     */
-    public function getCached($url)
-    {
-        return file_get_contents($this->urlToPath($url));
-    }
-    /**
-     * @inheritdoc
-     */
-    public function setCached($url, $content)
-    {
-        file_put_contents($this->urlToPath($url), $content);
-    }
-    /**
-     * @inheritdoc
-     */
-    public function setTempPath($path)
-    {
-        if (!is_dir($path)) {
-            mkdir($path);
-        }
+	protected $tmp;
+	public function __construct()
+	{
+		$this->tmp = sys_get_temp_dir();
+	}
+	private function urlToPath($url)
+	{
+		$dir = $this->tmp . DIRECTORY_SEPARATOR . "OpenWeatherMapPHPAPI";
+		if (!is_dir($dir)) {
+			mkdir($dir);
+		}
+		$path = $dir . DIRECTORY_SEPARATOR . md5($url);
+		return $path;
+	}
+	/**
+	 * @inheritdoc
+	 */
+	public function isCached($url)
+	{
+		$path = $this->urlToPath($url);
+		if (!file_exists($path) || filectime($path) + $this->seconds < time()) {
+			//echo "Weather data is NOT cached!\n";
+			return false;
+		}
+		//echo "Weather data is cached!\n";
+		return true;
+	}
+	/**
+	 * @inheritdoc
+	 */
+	public function getCached($url)
+	{
+		return file_get_contents($this->urlToPath($url));
+	}
+	/**
+	 * @inheritdoc
+	 */
+	public function setCached($url, $content)
+	{
+		file_put_contents($this->urlToPath($url), $content);
+	}
+	/**
+	 * @inheritdoc
+	 */
+	public function setTempPath($path)
+	{
+		if (!is_dir($path)) {
+			mkdir($path);
+		}
 
-        $this->tmp = $path;
-    }
+		$this->tmp = $path;
+	}
 }
